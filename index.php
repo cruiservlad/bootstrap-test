4d058ea6afc4e100d2edc350c1ead901f7d067f9
Pisarev Vladislav <pisarevvlad@gmail.com>
2017-10-08 18:59:07 +0300

    first init

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<form method="post">
+  <input type='text' name='email'>
+  <input type='text' name='password'>
+  <button></button>
+</form>
+<?php
+  if(!empty($_POST['email'])){
+    $db = mysql_connect("localhost", "root", "");
+    mysql_select_db("test", $db);
+    $query = "INSERT INTO `users` (`email`, `pass`) VALUES('".$_POST['email']."', '".$_POST['password']."')";
+    mysql_query($query);
+  }
+ ?>
